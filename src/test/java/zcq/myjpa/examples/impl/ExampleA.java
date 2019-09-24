44439a84ab6cb6c6f80b033e5ce537cd756ce35b
zchuqin <z.chuqin@gmail.com>
2019-09-24 18:20:56 +0800

    Utils

--- src/test/java/zcq/myjpa/examples/impl/ExampleA.java
+++ src/test/java/zcq/myjpa/examples/impl/ExampleA.java
@@ -22,10 +22,13 @@ public class ExampleA implements Example {
 
     @Override
     public void doing1(){
-        String[] strings = {"happy", "birthday", "hello"};
+        String[] strings = {"happy", "birthday", "hello","uu","id","vbee"};
 //        System.out.println(Arrays.stream(strings).filter((s) -> s.contains("y")).collect(Collectors.joining(",")));
-        try (Stream stream = Arrays.stream(strings).filter((s) -> s.contains("y"))) {
-            ((List) (stream.collect(Collectors.toList()))).forEach(System.out::println);
+        try (Stream<String> stream = Arrays.stream(strings)) {
+//            ((List) (stream.collect(Collectors.toList()))).forEach(System.out::println);
+//            System.out.println(stream.collect(Collectors.joining("-")));
+//            System.out.println(stream.mapToInt((s) -> s.length()).summaryStatistics().getCount());
+            stream.flatMap((s)-> Arrays.stream(s.split(""))).forEach(System.out::println);
         }
     }
 
